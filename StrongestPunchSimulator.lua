82d0cb6cf6ae03e3e207a42f5cae412940f7d2dd
userofwind <76060406+userofwind@users.noreply.github.com>
2021-09-30 20:14:30 -0500

    Create StrongestPunchSimulator.lua

---
+++ StrongestPunchSimulator.lua
@@ -0,0 +1,60 @@
+-- UI Setup
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/userofwind/LuxInc/master/LuxUILibrary.lua'))()
+local UI = Library:SetupUI('Strongest Punch Simulator')
+
+-- UI Hotkey
+UI:SetHotkey('q')
+
+-- Tabs
+local BoostsTab = UI:CreateTab('Boosts Farm')
+
+-- Services
+local WorkspaceService = game:GetService('Workspace')
+local PlayersService = game:GetService('Players')
+
+-- Variables
+local LocalPlayer = PlayersService.LocalPlayer
+local LocalPlayerCharacter = LocalPlayer.Character
+local AutoFarmingBoosts = false
+
+-- Boosts Tab
+BoostsTab:CreateButton(
+    'Regular Farm',
+    function ()
+        local CurrentWorld = LocalPlayer['PlayerGui']:FindFirstChild('ScreenGui'):FindFirstChild('Menu_Frame'):FindFirstChild('MapTeleportation_Frame'):FindFirstChild('MapLvl_Txt').Text:split(' ')[2]
+        
+        for _, Child in ipairs(WorkspaceService['Map']['Stages']['Boosts'][CurrentWorld]:GetDescendants()) do
+            if Child.ClassName == 'TouchTransmitter' then
+                firetouchinterest(LocalPlayerCharacter.HumanoidRootPart, Child.Parent, 0)
+                firetouchinterest(LocalPlayerCharacter.HumanoidRootPart, Child.Parent, 1)
+            
+                wait(0.2)
+            end
+        end
+    end
+)
+
+BoostsTab:CreateToggle(
+    'Auto Farm',
+    function ()
+        AutoFarmingBoosts = true
+        
+        local CurrentWorld = LocalPlayer['PlayerGui']:FindFirstChild('ScreenGui'):FindFirstChild('Menu_Frame'):FindFirstChild('MapTeleportation_Frame'):FindFirstChild('MapLvl_Txt').Text:split(' ')[2]
+
+        while AutoFarmingBoosts do
+            wait()
+            
+            local DiscoveredChild = WorkspaceService['Map']['Stages']['Boosts'][CurrentWorld]:GetDescendants()[1]:GetChildren()
+            
+            if DiscoveredChild.ClassName == 'TouchTransmitter' then
+                firetouchinterest(LocalPlayerCharacter.HumanoidRootPart, DiscoveredChild.Parent, 0)
+                firetouchinterest(LocalPlayerCharacter.HumanoidRootPart, DiscoveredChild.Parent, 1)
+            
+                wait(0.2)
+            end
+        end
+    end,
+    function ()
+        AutoFarmingBoosts = false
+    end
+)
