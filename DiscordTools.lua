d5535718f13c1d17207892445dc07868c4386383
userofwind <76060406+userofwind@users.noreply.github.com>
2021-10-01 11:33:51 -0500

    Update DiscordTools.lua

--- DiscordTools.lua
+++ DiscordTools.lua
@@ -7,16 +7,50 @@ UI:SetHotkey('g')
 
 -- Tabs
 local WebhookRaiderTab = UI:CreateTab('Webhook Raider')
-local ServerInfoTab = UI:CreateTab('Server Info')
-local UserInfoTab = UI:CreateTab('User Info')
+
+-- Services
+local HttpService = game:GetService('HttpService')
 
 -- Variables
-local WebhookURL = ''
+local MsgsToSend = 0
+local Request = {
+	Url = '',
+	Method = 'POST',
+	Body = {},
+	Headers = { ['Content-Type'] =  'application/json' }
+}	
 
 -- Webhook Raider Tab
 WebhookRaiderTab:CreateBox(
 	'Webhook URL',
 	function (Value)
-		WebhookURL = Value
+		Request.Url = Value
+	end
+)
+
+WebhookRaiderTab:CreateBox(
+	'Msg Content',
+	function (Value)
+		Request.Body = HttpService:JSONEncode { content = Value }
+	end
+)
+
+WebhookRaiderTab:CreateSlider(
+	'Msg Amount',
+	0,
+	100
+	function (Value)
+		MsgAmount = Value
+	end
+)
+
+WebhookRaiderTab:CreateButton(
+	'Send Raid',
+	function ()
+		pcall(function ()
+			for i = 0, MsgAmount do
+				syn.request(Request)
+			end
+		end)
 	end
 )
