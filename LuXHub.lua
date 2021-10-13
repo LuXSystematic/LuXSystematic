869c6eb32e83bdacea9f6a5bb538cc9db8220a3a
LuX Developer <87079094+muzrblx@users.noreply.github.com>
2021-10-13 13:38:32 -0400

    Update LuXHub.lua

--- LuXHub.lua
+++ LuXHub.lua
@@ -1,15 +1,1 @@
-if game.PlaceId == 4520749081 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/KingLegacy.lua'))()
-elseif game.PlaceId == 6381829480 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/KingLegacy.lua'))()
-elseif game.PlaceId == 6284583030 then
-    loadstring(game:HttpGet(''))()
-elseif game.PlaceId == 7426264451 then
-    loadstring(game:HttpGet(''))()
-elseif game.PlaceId == 7082539621 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/animedestroyersimulator.lua'))()
-elseif game.PlaceId == 7429434108 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZeroWareScripts/RobloxScripts/main/animetappers.lua'))()
-else
-    v:Kick("Game not supported")
-end
+
